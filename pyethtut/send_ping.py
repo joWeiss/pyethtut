549bad1edd6f2f69b97cf08f17ac12f6bc3d2a63
Jonas Weissensel <jweissensel@mailbox.org>
2017-07-23 13:33:55 +0200

    Pinging not local endpoint but official ethereum foundation bootnode

--- pyethtut/send_ping.py
+++ pyethtut/send_ping.py
@@ -5,7 +5,9 @@ if __name__ == '__main__':
     from discovery import EndPoint, PingServer
     my_ip = myip()
     my_endpoint = EndPoint(my_ip, 30303, 30303)
-    their_endpoint = EndPoint('127.0.0.1', 30303, 30303)
+    # ping to ethereum foundation go bootnode
+    # see https://ocalog.com/post/18/
+    their_endpoint = EndPoint('52.16.188.185', 30303, 30303)
 
     server = PingServer(my_endpoint)
 
